b26c933154c1d9fa6ba8402110056a1247e26542
Virgile Daugé <virgile.dauge@pm.me>
2024-10-31 13:24:14 +0100

    correction gen dates mois

--- src/stationreappropriation/utils.py
+++ src/stationreappropriation/utils.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from datetime import date
-
+from dateutil.relativedelta import relativedelta
 from calendar import monthrange
 from dotenv import load_dotenv
 
@@ -62,3 +62,32 @@ def gen_trimester_dates(trimester: int, current_year: int | None = None) -> tupl
     ending_date = date(current_year, end_month, monthrange(current_year, end_month)[1])
 
     return starting_date, ending_date
+def gen_previous_month_boundaries(current: date | None = None) -> tuple[date, date]:
+    if current is None:
+        current = date.today()
+    
+    # Move to the first day of the previous month
+    previous_month = current.replace(day=1) - relativedelta(months=1)
+    
+    # Use gen_month_boundaries to get the start and end dates
+    return gen_month_boundaries(previous_month)
+
+def gen_month_boundaries(current: date | None = None) -> tuple[date, date]:
+    if current is None:
+        current = date.today()
+    starting_date = current.replace(day=1)
+    ending_date = current.replace(day=monthrange(current.year, current.month)[1])
+    return starting_date, ending_date
+
+def gen_last_months(n:int=4) -> dict[str, tuple[date, date]]:
+    today = date.today()
+    dates = []
+    for i in range(n):
+        year = today.year if (today.month - i - 1) > 0 else today.year - 1
+        month = ((today.month - i - 1) % 12) or 12
+        # Get the last day of the month
+        _, last_day = monthrange(year, month)
+        # Use min to ensure we don't exceed the last day of the month
+        day = min(today.day, last_day)
+        dates.append(date(year, month, day))
+    return {f'{d.year}_{d.month:02}':gen_month_boundaries(d) for d in dates}
