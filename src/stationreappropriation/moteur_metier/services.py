99f3bad70830681e5958c480d2b9951bb78cfb1b
virgile <virgile.dauge@pm.me>
2025-02-04 14:48:56 +0100

    ajout dates par défault

--- src/stationreappropriation/moteur_metier/services.py
+++ src/stationreappropriation/moteur_metier/services.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from .consos import qui_quoi_quand, ajout_R151, calcul_energie
+from .consos import qui_quoi_quand, ajout_R151, ajout_par_defaut, calcul_energie
 from .turpe import get_applicable_rules, compute_turpe
 from .formattage import supprimer_colonnes, fusion_des_sous_periode
 
@@ -10,7 +10,8 @@ def energies_et_taxes(deb: pd.Timestamp, fin: pd.Timestamp, c15: pd.DataFrame, r
     
     alors = qui_quoi_quand(deb, fin, c15)
     indexes = ajout_R151(deb, fin, alors, r151)
-    energies = calcul_energie(indexes)
+    complet = ajout_par_defaut(deb, fin, indexes)
+    energies = calcul_energie(complet)
     energies['Puissance_Souscrite'] = pd.to_numeric(energies['Puissance_Souscrite'])
     rules = get_applicable_rules(deb, fin)
     turpe = compute_turpe(entries=energies, rules=rules)
