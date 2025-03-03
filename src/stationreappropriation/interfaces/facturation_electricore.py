f4fe5cb3c4f57c1f373df760f225997b0ca9f139
virgile <virgile.dauge@pm.me>
2025-03-03 10:08:50 +0100

    ajout prep et serialisation avnat envoi

--- src/stationreappropriation/interfaces/facturation_electricore.py
+++ src/stationreappropriation/interfaces/facturation_electricore.py
@@ -105,6 +105,15 @@ def _(mo):
 
 
 @app.cell
+def _(deb, fin, historique):
+    from electricore.core.périmètre.fonctions import extraire_historique_à_date, extraire_modifications_impactantes
+
+    mci = extraire_modifications_impactantes(deb=deb, historique=extraire_historique_à_date(fin=fin, historique=historique))
+    mci
+    return extraire_historique_à_date, extraire_modifications_impactantes, mci
+
+
+@app.cell
 def _(deb, fin, historique, relevés):
     from electricore.core.services import facturation
     factu = facturation(deb, fin, historique, relevés)
@@ -147,14 +156,65 @@ def _(env, mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Fusion des données Enedis et Odoo""")
+    mo.md(r"""# Fusion des données Enedis et Odoo""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Préparation des données métier
+
+        Cette adaptation est nécessaire pour connecter la nouvelle version de electricore à la partie Odoo de stationréappropriation. Il y aura une correspondance directe plus tard.
+        """
+    )
     return
 
 
 @app.cell
-def _(draft_orders, end_date_picker, start_date_picker, taxes):
-    _required_cols = ['HP', 'HC', 'BASE', 'j', 'd_date', 'f_date', 'Type_Compteur', 'Num_Compteur', 'Num_Depannage', 'pdl', 'turpe_fix', 'turpe_var', 'turpe', 'missing_data']
-    merged_data = draft_orders.merge(taxes[_required_cols], left_on='x_pdl', right_on='pdl', how='left')
+def _(factu):
+    required_cols = ['HP', 'HC', 'BASE', 'j', 'd_date', 'f_date', 'Type_Compteur', 'Num_Compteur', 'Num_Depannage', 'pdl', 'turpe_fix', 'turpe_var', 'turpe', 'missing_data']
+
+    # On renomme quelques trucs pour se faciliter la vie pour l'instant : 
+
+    métier = factu.copy()
+    métier = métier.rename(columns={
+        'Date_Releve_deb': 'd_date',
+        'Date_Releve_fin': 'f_date',
+        'Energie_Calculee': 'missing_data',
+        'turpe_fixe': 'turpe_fix'})
+    import pandera as pa
+    from typing import List, Optional
+    class ModèleMétier(pa.DataFrameModel):
+        HP: pa.typing.Series[float] = pa.Field(nullable=True)
+        HC: pa.typing.Series[float] = pa.Field(nullable=True)
+        BASE: pa.typing.Series[float] = pa.Field(nullable=True)
+        j: pa.typing.Series[int] = pa.Field(nullable=True)
+        d_date: pa.typing.Series = pa.Field() # Jsais plus ce qui est attendu
+        f_date: pa.typing.Series = pa.Field() # Jsais plus ce qui est attendu
+        Type_Compteur: pa.typing.Series[str] = pa.Field()
+        Num_Compteur: pa.typing.Series[str] = pa.Field()
+        Num_Depannage: pa.typing.Series[str] = pa.Field()
+        pdl: pa.typing.Series[str] = pa.Field()
+        turpe_fix: pa.typing.Series[float] = pa.Field(nullable=True)
+        turpe_var: pa.typing.Series[float] = pa.Field(nullable=True)
+        turpe: pa.typing.Series[float] = pa.Field(nullable=True)
+        missing_data: pa.typing.Series[bool] = pa.Field()
+
+    métier = ModèleMétier.validate(métier)
+    return List, ModèleMétier, Optional, métier, pa, required_cols
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Fusion données métier et données facturation""")
+    return
+
+
+@app.cell
+def _(draft_orders, end_date_picker, métier, start_date_picker, taxes):
+    merged_data = draft_orders.merge(taxes[métier], left_on='x_pdl', right_on='pdl', how='left')
     days_in_month = (end_date_picker.value - start_date_picker.value).days
     merged_data['update_dates'] = merged_data['j'] != days_in_month
     merged_data['missing_data'] = merged_data['missing_data'].astype(bool).fillna(True)
@@ -162,6 +222,182 @@ def _(draft_orders, end_date_picker, start_date_picker, taxes):
 
     merged_data
     return days_in_month, merged_data
+
+
+@app.cell
+def _(mo):
+    red_button = mo.ui.run_button(kind='danger', label=f'Écrire dans la base Odoo')
+    return (red_button,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""# Préparation des données avant envoi""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Abonnements""")
+    return
+
+
+@app.cell
+def _(merged_data, mo, np, pd):
+    _orders = pd.DataFrame(merged_data['sale.order_id'].copy())
+    _orders['x_invoicing_state'] = np.where(np.random.rand(len(_orders)) < 0.1, 'populated', 'checked')
+    _orders.loc[merged_data['something_wrong'] == True, 'x_invoicing_state'] = 'draft'
+    _orders.rename(columns={'sale.order_id':'id'}, inplace=True)
+    orders = _orders.to_dict(orient='records')
+    mo.accordion({
+        'Abonnements':_orders, 
+    })
+    return (orders,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""### Factures""")
+    return
+
+
+@app.cell
+def _(merged_data, mo):
+    _invoices = merged_data[['last_invoice_id', 
+                            'turpe', 
+                            'd_date', 'f_date', 
+                            'Type_Compteur', 
+                            'Num_Compteur']].copy()
+
+    _invoices['d_date'] = _invoices['d_date'].dt.strftime('%Y-%m-%d')
+    _invoices['f_date'] = _invoices['f_date'].dt.strftime('%Y-%m-%d')
+
+    _to_rename = {
+        'last_invoice_id':'id',
+        'turpe':'x_turpe',
+        'd_date':'x_start_invoice_period',
+        'f_date':'x_end_invoice_period',
+        'Type_Compteur':'x_type_compteur',
+        'Num_Compteur':'x_num_serie_compteur',
+    }
+
+    _invoices.rename(columns=_to_rename, inplace=True)
+    invoices = _invoices.to_dict(orient='records')
+
+    mo.accordion({
+        'Factures':_invoices, 
+    })
+    return (invoices,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Lignes de facture""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Énergies Consommées
+
+        On ne met à jour que les non-lissés, dont on dispose des données.
+        """
+    )
+    return
+
+
+@app.cell
+def _(merged_data, pd):
+    # On s'intéresse uniquement les données d'énergies qu'il faut mettre à jour
+    update_conso_df = merged_data[(~merged_data['x_lisse']) & (merged_data['something_wrong']==False)].copy()
+
+    hc = pd.DataFrame()
+    hp = pd.DataFrame()
+    base = pd.DataFrame()
+    if 'line_id_HC' in update_conso_df.columns:
+        hc = update_conso_df[update_conso_df['line_id_HC'].notna()][['line_id_HC', 'HC']]
+        hc = hc.dropna(subset=['HC'])
+        hc.rename(columns={'line_id_HC': 'id', 'HC': 'quantity'}, inplace=True)
+
+    if 'line_id_HP' in update_conso_df.columns:
+        hp = update_conso_df[update_conso_df['line_id_HP'].notna()][['line_id_HP', 'HP']]
+        hp = hp.dropna(subset=['HP'])
+        hp.rename(columns={'line_id_HP': 'id', 'HP': 'quantity'}, inplace=True)
+    
+    if 'line_id_Base' in update_conso_df.columns:
+        base = update_conso_df[update_conso_df['line_id_Base'].notna()][['line_id_Base', 'BASE']]
+        base = base.dropna(subset=['BASE'])
+        base.rename(columns={'line_id_Base': 'id', 'BASE': 'quantity'}, inplace=True)
+    
+    return base, hc, hp, update_conso_df
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Jours d'abonnement
+
+        On ne met à jour que si pas lissé ou lissé entré ou sorti dans la période
+        """
+    )
+    return
+
+
+@app.cell
+def _(merged_data):
+    do_update_qty = (~(merged_data['x_lisse'] == True) | (merged_data['update_dates'] == True))
+    abo = merged_data[do_update_qty][['line_id_Abonnements', 'j']]
+    abo = abo.dropna(subset=['line_id_Abonnements', 'j'])
+    abo.rename(columns={'line_id_Abonnements': 'id', 'j': 'quantity'}, inplace=True)
+    return abo, do_update_qty
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## Convertion DataFrame vers une liste de lignes
+
+        On va ajouter toutes les lignes, hp, hc, base et abonnement dans une même liste, qui sera ensuite envoyée d'un bloc à Odoo.
+        """
+    )
+    return
+
+
+@app.cell
+def _(abo, base, hc, hp):
+    lines = []
+    lines += base.to_dict(orient='records')
+    lines += hp.to_dict(orient='records')
+    lines += hc.to_dict(orient='records')
+    lines += abo.to_dict(orient='records')
+    return (lines,)
+
+
+@app.cell
+def _(lines, mo):
+    mo.accordion({'Lignes de factures': lines})
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Visualisation des données à envoyer""")
+    return
+
+
+@app.cell
+def _(abo, base, hc, hp, mo):
+    mo.accordion({
+        'HC':hc, 
+        'HP':hp, 
+        'base':base, 
+        'jours':abo
+    })
+    return
 
 
 if __name__ == "__main__":
